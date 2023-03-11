253a0746b964af4a68218c627271fa9ed5ed80b7
jdrohan356 <jdrohan356@gmail.com>
2023-03-11 16:30:18 -0500

    Added New column

--- data_exploration.py
+++ data_exploration.py
@@ -1,8 +1,24 @@
 import pandas as pd
 import matplotlib as plt
 from datetime import datetime
+import re
 
 filename = 'netflix_titles.csv'
+
+def calc_complexity(string):
+    ''' Automated Readability Index '''
+
+    string = string.lower()
+
+    sentence_count = max([len(string.split('.')[:-1]), 1])
+    word_count = len(string.split())
+    char_count = len(re.sub(r'\W+', '', string))
+
+    return round(4.71 * (char_count / word_count) + 0.5 * (word_count / sentence_count) - 21.43)
+
+
+
+
 
 def main():
 
@@ -10,13 +26,15 @@ def main():
     netflix_df.set_index('show_id', inplace=True)
     netflix_df.dropna(inplace=True)
     netflix_df = netflix_df[(netflix_df['country'] == 'United States') & (netflix_df['type'] == 'Movie')]
-    netflix_df.drop(columns=['description', 'country', 'type'], inplace=True)
+    netflix_df.drop(columns=['country', 'type'], inplace=True)
 
     netflix_df['duration'] = list(map(lambda x: x.split()[0], netflix_df['duration']))
-    netflix_df['date added'] = list(map(lambda x: datetime.strptime(x, '%m-%d-%Y').date(), netflix_df['date added']))
+    netflix_df['date_added'] = list(map(lambda x: datetime.strptime(x, '%B %d, %Y').date(),
+                                        netflix_df['date_added']))
 
-    print(netflix_df['date added'])
+    netflix_df['complexity'] = list(map(calc_complexity, netflix_df['description']))
 
+    print(netflix_df)
 
 
 
